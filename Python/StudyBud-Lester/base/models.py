ae8d6ee754654ba71009c79c64459c7ff16a675b
LesterBryanIlao <lestertetengilao@gmail.com>
2022-08-07 21:58:06 +0800

    Finished Activity Feed.

--- Python/StudyBud-Lester/base/models.py
+++ Python/StudyBud-Lester/base/models.py
@@ -14,12 +14,13 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants = 
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True)       # time stamp for every update
     created = models.DateTimeField(auto_now_add=True)   # time stamp when created
     
     class Meta:
         ordering =['-updated', '-created']
+        
     def __str__(self):
         return str(self.name)
 
@@ -30,5 +31,8 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)       # time stamp for every update
     created = models.DateTimeField(auto_now_add=True)   # time stamp when created
     
+    class Meta:
+        ordering =['-updated', '-created']
+        
     def __str__(self):
         return self.body[:50]
--- Python/StudyBud-Lester/base/views.py
+++ Python/StudyBud-Lester/base/views.py
@@ -1,4 +1,5 @@
 import re
+from tkinter.messagebox import Message
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.db.models import Q
@@ -7,7 +8,7 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # Create your views here.
@@ -79,14 +80,28 @@ def home(request):
     
     topics = Topic.objects.all()
     room_count = rooms.count()
-    context = {'rooms':rooms, 'topics': topics, 'room_count': room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    
+    context = {'rooms':rooms, 'topics': topics, 
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all().order_by('-created')
-    context = {'room': room, 'room_messages': room_messages}
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+    
+    context = {'room': room, 'room_messages': room_messages, 
+               'participants': participants}
     return render(request, 'base/room.html', context)
 
 
@@ -134,4 +149,18 @@ def deleteRoom(request, pk):
         return redirect('home')
     
     context={'obj': room}
+    return render(request, 'base/delete.html', context)
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse('Your are not allowed here!!')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    
+    context={'obj': message}
     return render(request, 'base/delete.html', context)
