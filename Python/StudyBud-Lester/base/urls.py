b765c5536b60454e3dedbbd36f4a7d2c88058ff9
LesterBryanIlao <lestertetengilao@gmail.com>
2022-08-17 20:42:46 +0800

    Mobile Responsiveness Done!

--- Python/StudyBud-Lester/base/urls.py
+++ Python/StudyBud-Lester/base/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('update-user/', views.updateUser, name="update-user"),
     
     path('topcis/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
     
 ]
